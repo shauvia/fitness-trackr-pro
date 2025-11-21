8a74f44d2a0821338be91ed9ff6e7009ea96b1a4
Ewa Szalwinska <e.szalwinska@gmail.com>
2025-11-20 18:58:13 -0800

    last changes to App

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ export default function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
+        <Route path="/" element={<ActivitiesPage />} />
         <Route path="/activities" element={<ActivitiesPage />} />
         <Route path="/activityList/:activityId" element={<ActivityDetails />} />
         <Route path="/login" element={<Login />} />
